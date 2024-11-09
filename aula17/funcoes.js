a6686cedb610e9bb16fb931d71d675f512caf3b2
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2024-11-09 14:30:53 -0300

    Funções

---
+++ aula17/funcoes.js
@@ -0,0 +1,37 @@
+// function saudacao(nome) {
+//     console.log(`Bom dia ${nome}!`);
+//     return true
+// }
+
+// const variavel = saudacao('Vinícius');
+// console.log(variavel);
+
+// function saudacao(nome) {
+//     return `Bom dia ${nome}!`
+// }
+
+// const variavel = saudacao('Vinícius');
+// console.log(variavel);
+
+// function soma(x = 1, y = 1) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// const resultado = soma(4, 2);
+// console.log(resultado);
+
+// const raiz = function (n) {
+//     return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+

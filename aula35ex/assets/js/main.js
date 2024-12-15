0fc54479ae1f23f351aee1441747d0bf49a9a2fb
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2024-12-15 13:22:32 -0300

    Resolução da aula35ex
    Exercício concluído com sucesso, feito totalmente por mim.

--- aula35ex/assets/js/main.js
+++ aula35ex/assets/js/main.js
@@ -1,10 +1,4 @@
-/*
-1 - Funções que iram criar as tags HTML
-2- Separar as frases
-3 - Colocar as frases nas tags
-4 - Colocar as tags com as frases no HTMl
-*/
-
+//* const com um array de quatro objetos, e cada objeto com uma tag e uma frase
 const elementos = [
     {tag: 'p', texto: 'Frase 1'},
     {tag: 'div', texto: 'Frase 2'},
@@ -12,27 +6,18 @@ const elementos = [
     {tag: 'section', texto: 'Frase 4'},
 ];
 
-for(let i = 0; i < elementos.length; i++){
-    const objetos = elementos[i];
-    // const {tag} = objetos;
-    const {texto} = objetos;
-    
-    document
-}
+//* const que seleciona o container(onde vai ir as tags com as frases) no HTML
+const cont = document.querySelector('.container');
 
- function criarP(){
-    const p = document.createElement('p');
-    return p;
- }
- function criarDiv(){
-    const div = document.createElement('div');
-    return div;
- }
- function criarFooter(){
-    const footer = document.createElement('footer');
-    return footer;
- }
-function criarSection(){
-    const section = document.createElement('section');
-    return section;
+//* elemento de repetição com base no número de objetos que tem no array de elementos
+for(let i = 0; i < elementos.length; i++){
+
+//* consts que irão pegar as tags e textos e separar em objetos
+    let {tag} = elementos[i];
+    let {texto} = elementos[i];
+
+//* const que irar criar as tags e adicionar as frases com base no valor de i na sequência que aparece no array de elementos
+    const criarTags = document.createElement(tag);
+    cont.appendChild(criarTags).innerHTML = texto;
+    console.log(i,tag,texto); //! console.log() normal para mostrar no console do navegador os valores
 }

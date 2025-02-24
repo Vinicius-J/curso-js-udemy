4f2a0dc65f77f5d92c2b8d6fce4226450dd3e6f1
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-02-24 09:42:11 -0300

    Objeto Map - new Map()

---
+++ aula80/objeto-map.js
@@ -0,0 +1,28 @@
+const pessoas = [
+  { id: 3, nome: 'Vinícius' },
+  { id: 2, nome: 'Maria' },
+  { id: 1, nome: 'Helena' },
+]
+
+// const novasPessoas = {};
+// for (const pessoa of pessoas) {
+//   const { id } = pessoa;
+//   novasPessoas[id] = { ...pessoa };
+// }
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+  const { id } = pessoa;
+  novasPessoas.set(id, { ...pessoa });
+}
+
+// for(const [identifier, {id, nome}] of novasPessoas){
+//   console.log(identifier, id, nome);
+// }
+
+// for(const pessoa of novasPessoas.values()){
+//   console.log(pessoa)
+// }
+
+novasPessoas.delete(2);
+console.log(novasPessoas);

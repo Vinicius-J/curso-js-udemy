947502e4adcfb637cc3f83b231f4305194098625
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-04-09 19:29:34 -0300

    Sobre Módulos 2

---
+++ node/aula02/app.js
@@ -0,0 +1,15 @@
+// const Cachorro = require('./Z/mod2');
+
+// const c1 = new Cachorro('Jade');
+// c1.latir();
+// console.log(c1);
+
+// const path = require('path');
+// console.log(path.resolve(__dirname, '..', '..','arquivos', 'imagens'));
+
+// console.log(__filename); //* arquivo atual
+// console.log(__dirname); //* pasta atual
+
+const Cachorro = require('./Z/mod2');
+const c1 = new Cachorro('Dog');
+console.log(c1);

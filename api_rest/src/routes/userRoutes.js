31caa0b8e4b65e59d9168f49f2a11495f6889e03
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-10-09 10:38:46 -0300

    CRUD de usuários

--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import userController from "../controllers/UserController.js";
+
 const router = new Router();
+
 router.post("/", userController.store);
+router.get("/", userController.index);
+router.get("/:id", userController.show);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.delete);
+
 export default router;
 
 /*

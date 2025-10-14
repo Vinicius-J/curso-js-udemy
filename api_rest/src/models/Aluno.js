ae8432dc0ebf430701fabc787d8b8d3c816dfb7a
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-10-14 16:24:15 -0300

    CRUD de alunos

--- api_rest/src/models/Aluno.js
+++ api_rest/src/models/Aluno.js
@@ -4,12 +4,65 @@ export default class Aluno extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: DataTypes.STRING,
-        sobrenome: DataTypes.STRING,
-        email: DataTypes.STRING,
-        idade: DataTypes.INTEGER,
-        peso: DataTypes.FLOAT,
-        altura: DataTypes.FLOAT,
+        nome: {
+          type: DataTypes.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: "Nome precisa ter entre 3 e 255 caracteres.",
+            },
+          },
+        },
+        sobrenome: {
+          type: DataTypes.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: "Sobrenome precisa ter entre 3 e 255 caracteres.",
+            },
+          },
+        },
+        email: {
+          type: DataTypes.STRING,
+          defaultValue: "",
+          unique: {
+            msg: "E-mail já existe.",
+          },
+          validate: {
+            isEmail: {
+              msg: "E-mail inválido.",
+            },
+          },
+        },
+        idade: {
+          type: DataTypes.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Idade precisa ser um número inteiro.",
+            },
+          },
+        },
+        peso: {
+          type: DataTypes.FLOAT,
+          defaultValue: "",
+          validate: {
+            isFloat: {
+              msg: "Peso precisa ser um número inteiro ou de ponto flutuante.",
+            },
+          },
+        },
+        altura: {
+          type: DataTypes.FLOAT,
+          defaultValue: "",
+          validate: {
+            isFloat: {
+              msg: "Altura precisa ser um número inteiro ou de ponto flutuante.",
+            },
+          },
+        },
       },
       {
         sequelize,

66d1d8e68351daccc23da3aa71b2a5206381bb42
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-10-15 20:08:27 -0300

    Arquivos estáticos

--- api_rest/src/models/Foto.js
+++ api_rest/src/models/Foto.js
@@ -1,4 +1,5 @@
 import { DataTypes, Model } from "sequelize";
+import appConfig from "../config/appConfig";
 
 export default class Foto extends Model {
   static init(sequelize) {
@@ -20,6 +21,12 @@ export default class Foto extends Model {
             notEmpty: {
               msg: "Campo não pode ficar vazio.",
             },
+          },
+        },
+        url: {
+          type: DataTypes.VIRTUAL,
+          get() {
+            return `${appConfig.url}/images/${this.getDataValue("filename")}`;
           },
         },
       },

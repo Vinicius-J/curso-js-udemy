2e279d23b48fa5fd225e3cb71acd4eecd56befab
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-10-24 18:36:52 -0300

    update .env port

--- api_rest/src/config/database.js
+++ api_rest/src/config/database.js
@@ -25,6 +25,7 @@ module.exports = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: "mysql",
   },
 };

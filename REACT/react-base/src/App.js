329c505663c5462e578e5f8d002f35588b78ef5d
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-11-05 18:09:43 -0300

    React Router DOM

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyles';
 
-import Login from './pages/Login';
 import Header from './components/Header/';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 }
 

4ea0ecf25b51797f6e710e550f645fbaf33f4d88
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-11-06 09:49:32 -0300

    React Toastify

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
+  toast.success('Sucesso!');
+  toast.error('Erro!');
   return (
     <Container>
       <Title>

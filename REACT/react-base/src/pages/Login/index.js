5a090bd260bec4ea31b1d588f19967beff76e06d
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-11-06 10:03:23 -0300

    Configurando o Axios

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
-  toast.success('Sucesso!');
-  toast.error('Erro!');
   return (
     <Container>
       <Title>

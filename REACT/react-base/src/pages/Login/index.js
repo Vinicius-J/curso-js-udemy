ec21d6537d37b698d41581cfec12818135022cba
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2025-11-06 11:09:05 -0300

    Separando Actions e Reducers

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -3,23 +3,19 @@ import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+import * as exampleActions from '../../store/modules/example/actions';
 
 export default function Login() {
   const dispatch = useDispatch();
 
   function handleClick(e) {
     e.preventDefault();
-    dispatch({
-      type: 'BOTAO_CLICADO',
-    });
+    dispatch(exampleActions.clicaBotao());
   }
 
   return (
     <Container>
-      <Title>
-        Login
-        <small>Oie</small>
-      </Title>
+      <Title>Login</Title>
       <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
       <button type="button" onClick={handleClick}>
         Enviar
